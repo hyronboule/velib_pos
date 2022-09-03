a126d6f7d1e057d56ee23fdb14a9d3cee62e52e4
cassandra <meteaucassandra@gmail.com>
2022-09-03 14:03:02 +0200

    init db table velib working

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import sqlite3
+import pandas as pd
+
+def initVelibTable(conn):
+    """
+    Delete all rows in the data in table
+    """
+    try:
+        sql = 'DROP TABLE IF EXISTS velib;'
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()    
+    except:
+        sql = 'CREATE TABLE velib'
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'coucou c cass'
+connection = sqlite3.connect("db.sqlite") # change to 'sqlite:///your_filename.db'
+
+ 
+initVelibTable(connection)
+
+
+df = pd.read_csv('ressources/velib-pos.csv', sep=";", low_memory=False)
+df.to_sql("velib", connection, if_exists='append', index=False)
+connection.commit()
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    return jsonify(data)   
+
 
 app.run(host='0.0.0.0', port=9090)
